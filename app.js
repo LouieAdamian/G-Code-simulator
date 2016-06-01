e6eeae8116c529bed0e95662a42a29f077d3bbe8
Louie Adamian <louieadamian@gmail.com>
2016-06-01 14:45:28 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var app = require('express')();
+var printerState = {
+    extruder - temp
+    bed - temp
+    x
+    y
+    z
+    xMin
+    xMax
+    yMin
+    yMax
+    zMin
+    zMax
+    extruderSteps
+}
+
+
+while(!eof()){
+var code =f.readline()
+if(code = G1){
+
+}
+
+}
+function G1(code) {
+
+}
+
+function G92(code) {
+
+}
+function M106(code) {
+
+}
