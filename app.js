76c9fb7a6ecae8b9c104d0f8241a711a5a0b180c
Louie Adamian <louieadamian@gmail.com>
2016-06-02 14:53:02 -0400

    adds function

--- app.js
+++ app.js
@@ -12,76 +12,57 @@
       zMin: 000,
       zMax: 800,
       extruderSteps: 0
-  }
-  var gcodefile = "gcode/" + "teensy" + ".gcode";
-  var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream(gcodefile)
-  });
+}
 
-  lineReader.on('line', function (line) {
-    console.log(line);
+while (gcode) {
+    var code = {}
+    if (code = G28) {}
+    if (code = G1) {}
+    if (code = G92) {}
+    if (code = M84) {}
+    if (code = M107) {}
+    if (code = G90) {}
+}
 
-
-  });
-
-  // fs.readFile("gcode/" + "teensy" + ".gcode", "utf8", function(err,data) {
-  //     if (err) {
-  //         console.log(err);
-  //     }
-  //     console.log('Line from file:',data);
-  //     console.log('AHHHHH!');
-  // })
-
-/*
-  while(gcode) {
-      var code = //read single line of gcode
-      if (code = G28) {}
-      if (code = G1) {}
-      if (code = G92) {}
-      if (code = M84) {}
-      if (code = M107) {}
-      if (code = G90) {}
-  }
-
-  function G1(code) {
-      if (code = X) {
-          var X = code;
-      }
-      if (code = Y) {
-          var Y = code;
-      }
-      if (code = Z) {
-          var Z = code
-      }
-      if (code = E) {
-          var E = code
-      }
-      if (code = F) {}
-  }
-
-  function G92(code) {
-      if (code = X) {}
-      if (code = Y) {}
-      if (code = Z) {}
-      if (code = E) {}
-  }
-
-  function M106(code) {
-      if (code = S) {}
-      if (code  = P1) {
-
-      }
-  }
-*/
-
-function readGcodeFile(file){
-fs.readFile("gcode/" + file + ".gcode", "utf8", function(err,data) {
-    if (err) {
-        console.log(err);
+function G1(code) {
+    if (code = X) {
+        var X = code;
     }
+    if (code = Y) {
+        var Y = code;
+    }
+    if (code = Z) {
+        var Z = code
+    }
+    if (code = E) {
+        var E = code
+    }
+    if (code = F) {}
+}
+
+function G92(code) {
+    if (code = X) {}
+    if (code = Y) {}
+    if (code = Z) {}
+    if (code = E) {}
+}
+
+function M106(code) {
+    if (code = S) {}
+    if (code = P1) {
+
+    }
+}
+
+function readGcodeFile(file) {
+    var gcodefile = "gcode/" + file + ".gcode";
+    var lineReader = require('readline').createInterface({
+        input: require('fs').createReadStream(gcodefile)
+    });
+
+    lineReader.on('line', function(line) {
+        console.log(line);
 
 
-
-
-})
+    });
 }
