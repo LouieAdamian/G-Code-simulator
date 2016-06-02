f7153264a6486e7c2e3398f5038c60c90aff2238
Louie Adamian <louieadamian@gmail.com>
2016-06-02 09:58:54 -0400

    starts function parser

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
 var app = require('express')();
 var printerState = {
-    extruder - temp
-    bed - temp
-    x
-    y
-    z
-    xMin
-    xMax
-    yMin
-    yMax
-    zMin
-    zMax
-    extruderSteps
+    extruderTemp;
+    bedTemp;
+    x;
+    y;
+    z;
+    xMin;
+    xMax;
+    yMin;
+    yMax;
+    zMin;
+    zMax;
+    extruderSteps;
 }
 
 
-while(!eof()){
-var code =f.readline()
-if(code = G1){
-
+while (!eof()) {
+    var code = f.readline()
+    if (code = G1) {}
+    if (code = G28) {}
+    if (code = G92) {}
+    if (code = M84) {}
+    if (code = M107) {}
 }
 
-}
 function G1(code) {
-
+    if (code = X) {}
+    if (code =Y){}
+    if (code =Z){}
+    if (code =E){}
+    if (code =F){}
 }
 
 function G92(code) {
 
 }
+
 function M106(code) {
+
+}
+
+function G92() {
 
 }
