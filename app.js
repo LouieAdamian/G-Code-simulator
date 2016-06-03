e903bb33bdba9870646c452de3be7eb18221e3eb
Louie Adamian <louieadamian@gmail.com>
2016-06-03 09:17:09 -0400

    rearrange app.js

--- app.js
+++ app.js
@@ -1,68 +1,58 @@
   var fs = require('fs');
-  var prvarerState = {
-      extruderTemp: 215,
-      bedTemp: 85,
-      x: 100,
-      y: 100,
-      z: 100,
-      xMin: 000,
-      xMax: 400,
-      yMin: 000,
-      yMax: 500,
-      zMin: 000,
-      zMax: 800,
-      extruderSteps: 0
-}
-
-while (gcode) {
-    var code = {}
-    if (code = G28) {}
-    if (code = G1) {}
-    if (code = G92) {}
-    if (code = M84) {}
-    if (code = M107) {}
-    if (code = G90) {}
-}
-
-function G1(code) {
-    if (code = X) {
-        var X = code;
+  
+  function ParseGcode() {
+    while (gcode) {
+        var code = {}
+        if (code = G28) {}
+        if (code = G1) {}
+        if (code = G92) {}
+        if (code = M84) {}
+        if (code = M107) {}
+        if (code = G90) {}
     }
-    if (code = Y) {
-        var Y = code;
+    var printersState = {
+        extruderTemp: 215,
+        bedTemp: 85,
+        x: 100,
+        y: 100,
+        z: 100,
+        xMin: 000,
+        xMax: 400,
+        yMin: 000,
+        yMax: 500,
+        zMin: 000,
+        zMax: 800,
+        extruderSteps: 0
     }
-    if (code = Z) {
-        var Z = code
-    }
-    if (code = E) {
-        var E = code
-    }
-    if (code = F) {}
-}
+  }
 
-function G92(code) {
-    if (code = X) {}
-    if (code = Y) {}
-    if (code = Z) {}
-    if (code = E) {}
-}
+  function readGcodeFile(file) {
+      var gcodefile = "gcode/" + file + ".gcode";
+      var lineReader = require('readline').createInterface({
+          input: require('fs').createReadStream(gcodefile)
+      });
 
-function M106(code) {
-    if (code = S) {}
-    if (code = P1) {
+      lineReader.on('line', function(line) {
+          console.log(line);
 
-    }
-}
+      if (code = Z) {
+          var Z = code
+      }
+      if (code = E) {
+          var E = code
+      }
+      if (code = F) {}
+  }
 
-function readGcodeFile(file) {
-    var gcodefile = "gcode/" + file + ".gcode";
-    var lineReader = require('readline').createInterface({
-        input: require('fs').createReadStream(gcodefile)
-    });
+  function G92(code) {
+      if (code = X) {}
+      if (code = Y) {}
+      if (code = Z) {}
+      if (code = E) {}
+  }
 
-    lineReader.on('line', function(line) {
-        console.log(line);
-
-
-    });
-}
+  function M106(code) {
+      if (code = S) {}
+      if (code = P1) {
+      }
+  }
